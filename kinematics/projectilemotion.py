f176748c2d81651550c7898de947beb2bf65fbc0
50275 <68676339+50275@users.noreply.github.com>
2025-09-24 13:32:24 -0700

    Update projectilemotion.py

--- kinematics/projectilemotion.py
+++ kinematics/projectilemotion.py
@@ -7,6 +7,9 @@ def perform(velocity, angle):
   vx = velocity * cos(angle)
   vy = velocity * sin(angle)
   a = -9.8
+  # isnt this misuse of my own library?
+  t = k.kinematics3(vf=0, vi=vy, a=a, t="?")
+  t = t * 2
   print("Max height: %s" % k.kinematics2(vf=0, vi=vy, a=a, d="?"))
-  print("Total air time: %s" % k.kinematics3(vf=0, vi=vy, a=a, t="?"))
-  
+  print("Total air time: %s" % t)
+  print("Total distance: %s" % (vx * t))
